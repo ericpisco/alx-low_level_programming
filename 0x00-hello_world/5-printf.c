3c54dac753bf110ae8bb6088f7f750896d465e64
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-08 06:28:43 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,10 @@
  * Return: Always 0 (success)
  */
 
+
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
+
 }
