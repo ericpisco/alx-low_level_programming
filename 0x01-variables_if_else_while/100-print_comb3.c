ca59c6d7e22a86a2c6a747a4c7a5a8a46f76ac6f
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-09 08:16:19 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens '0'; tens <= '9'; tens++)
+	{
+		if (!((ones == tens) || (tens > ones)))
+		{
+			putchar(tens);
+			putchar(ones);
+			if (!(ones == '9' && tens == '8'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
