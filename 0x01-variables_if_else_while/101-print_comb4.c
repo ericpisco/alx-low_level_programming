769ee710eee6eb86d3f274058e498449598fc839
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-09 12:39:46 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '0'; hundreds++)
+	{
+		for (tens = '0'; tens <= '0'; tens++)
+		{
+			if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && hundreds == '7' && tens == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
+
+
+
