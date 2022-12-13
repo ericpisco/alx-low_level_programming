8fd09f30293c56b7e6ecb068658c91d8210f6301
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-13 05:16:31 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,18 +22,21 @@ void print_times_table(int n)
 				_putchar(32);
 				if (op <= 9)
 				{
+					_putchar(',');
 					_putchar(32);
 					_putchar(32);
 					_putchar(op + 48);
 				}
 				else if (op <= 99)
 				{
+					_putchar(',');
 					_putchar(32);
 					_putchar((op / 10) + 48);
 					_putchar((op / 10) + 48);
 				}
 				else
 				{
+					_putchar(',');
 					_putchar(((op / 100) % 10) + 48);
 					_putchar(((op / 10) % 10) + 48);
 					_putchar(((op % 10) + 48));
