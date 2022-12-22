6a1e453897ebce0f320a0def913d7f643ff8478a
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-22 10:52:29 -0800

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
- */
-
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
-/**
  * print_number - prints integers enters as parameters using putchar
  *  @n: integer to print
  *  Return: void
