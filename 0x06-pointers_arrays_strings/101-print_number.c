475e627b995db249f5360af7c84752fc121838cb
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-22 11:48:44 -0800

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,15 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	unsigned int n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
-	if (n1 / 10 != 0)
-	{
+	if ((n1 / 10) > 0)
 		print_number(n1 / 10);
-	}
+
 	_putchar((n1 % 10) + '0');
 }
