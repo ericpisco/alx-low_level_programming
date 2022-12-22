e0153785a52490cb1b5bfdfbd3e1a925b18557b2
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2022-12-22 11:28:27 -0800

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,25 +3,23 @@
 /**
  * print_number - prints integers enters as parameters using putchar
  *  @n: integer to print
- *  Return: void
+ *  Return: 0
  */
 
 void print_number(int n)
 {
 	unsigned int n1;
 
+	n1 = n;
+
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		n1 = -n;
 	}
-	else
+	if (n1 / 10 != 0)
 	{
-		n1 = n;
-	}
-	if (n1 / 10)
 		print_number(n1 / 10);
+	}
 	_putchar((n1 % 10) + '0');
 }
-
-
