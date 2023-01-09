bbb3c6ec19c8c860a713669f5f1dfe7c195bcab9
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-09 04:36:15 -0800

    This task 0 is so challenging

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _abs - check the code for Holberton School students
+ * @n: is an integer argument
+ * Return: 0 Always
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
