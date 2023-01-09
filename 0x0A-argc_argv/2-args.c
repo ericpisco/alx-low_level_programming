82180c8f04ca90923b1989053308130f6e53a0ec
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-09 06:26:13 -0800

    All tasks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg;
+
+	for (arg = 0; arg < argc; arg++)
+		printf("%s\n", argv[arg]);
+
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is Arg C arg V project ALX
