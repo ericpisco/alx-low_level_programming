760399d7e5e226d334b04cf1092ee54e3b4f5ed2
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-10 00:55:14 -0800

    Task 2 corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,11 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-	for (j = 0; s2[j] != '\0'; j++);
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
 	strout = malloc(sizeof(char) * (i + j + 1));
 
@@ -37,5 +39,5 @@ char *str_concat(char *s1, char *s2)
 		strout[k] = s2[j];
 
 	return (strout);
-}
+i}
 
