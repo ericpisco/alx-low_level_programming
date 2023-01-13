11c71b8fa29c834f68fb1837c6aa61d666f71f98
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-13 04:43:18 -0800

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * main - multiplies two positive numbers
  * @argc: n arguments
@@ -13,8 +14,8 @@ int main(int argc, char *argv[])
 	int i, j;
 
 	if (argc != 3)
-	{printf("Error\n");
-		exit(98);}
+	{ printf("Error\n");
+	exit(98);}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
