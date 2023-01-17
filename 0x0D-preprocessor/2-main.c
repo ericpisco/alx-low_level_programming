eb96e3fee2d17ce991ac7b6ff94b884d491bbe88
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-17 01:14:40 -0800

    Preprocessor Tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - preints the name of the file
+ * Return: 0 Always when success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+ This is Preprocessor ALX Project
