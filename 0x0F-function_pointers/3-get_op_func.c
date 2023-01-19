84cb59aff37184cc036e0485a48938b3d810171d
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-19 08:16:14 -0800

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
