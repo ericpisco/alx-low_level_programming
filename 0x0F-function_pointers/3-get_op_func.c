5374eef0ba97d1e61fe68b0eed594bc3a0f5901a
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-19 08:46:39 -0800

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
+
 
 /**
  * get_op_func - ...
@@ -11,14 +10,16 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i = 0;
+
+	int i;
 
 	i = 0;
 
