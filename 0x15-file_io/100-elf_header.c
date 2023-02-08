729d6bd806e243abd03cf9669e20438e9a4bb9a8
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-02-08 13:17:02 -0800

    Task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,17 @@
 #include "main.h"
+#include <elf.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
 
 /**
  * check_elf - Entry Point
