d78990f287844043e0a6aedbc70d90168f8c852d
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-12 11:24:02 -0700

    Error in the main file fixed

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,9 @@
+#include "hash_tables.h"
+
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -11,8 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
+		hash = ((hash << 5) + hash) + c;
+
 	return (hash);
 }
